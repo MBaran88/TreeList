a52a85c8ed419578ca0f942aa66383041a278296
MBaran88 <zdzibq@gmail.com>
2017-05-24 19:42:11 +0200

    commit #4

--- src/com/TreeList/Main.java
+++ src/com/TreeList/Main.java
@@ -1,5 +1,7 @@
 package com.TreeList;
 
+import sun.reflect.generics.tree.Tree;
+
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
@@ -12,13 +14,25 @@ public class Main {
         TreeItem i2 = new TreeItem("Ma");
         TreeItem i3 = new TreeItem("Ala");
 
-        HashSet<TreeItem> i1Children = new HashSet<TreeItem>();
+        /*HashSet<TreeItem> i1Children = new HashSet<TreeItem>();
         i1Children.add(i2);
         i1.setChildren(i1Children);
 
         HashSet<TreeItem> i2Children = new LinkedHashSet<TreeItem>();
         i2Children.add(i3);
-        i2.setChildren(i2Children);
+        i2.setChildren(i2Children);}*/
+
+
+        TreeItem Roland = new TreeItem("Roland ze Spychowa");
+        TreeItem Zbych = new TreeItem("Zbigniew ze Starachowic");
+        TreeItem Piotrko = new TreeItem("Piotr z Krakowa");
+
+
+        HashSet<TreeItem> FirstFamily = new HashSet<TreeItem>();
+        FirstFamily.add(Roland);
+        FirstFamily.add(Zbych);
+        FirstFamily.add(Piotrko);}
+
 
 //    TreeItem currentSingle = i1;
 //    while(currentSingle != null){
@@ -28,6 +42,7 @@ public class Main {
         public void printTreeItem(TreeItem item){
         System.out.println(item.getData());
 
+        if (item.getChildren() != null){
         Iterator iterator = item.getChildren().iterator();
         while(iterator.hasNext()){
             printTreeItem((TreeItem)iterator.next());
